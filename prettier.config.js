54641584c6114963002fbf18ee01885edf885949
Oded Zilbershlag <123odzil@gmail.com>
2025-04-29 14:49:19 +0300

    feat(knip): change schema of knip.jsonc to JSONC; improve prettier.config.js accordingly

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,4 @@
 /** @type {import('prettier').Config} */
 export default {
-  overrides: [
-    {
-      files: '*.jsonc',
-      options: {
-        trailingComma: 'none',
-      },
-    },
-  ],
   singleQuote: true,
 };
